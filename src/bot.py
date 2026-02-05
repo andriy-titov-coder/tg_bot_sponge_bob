ac71747e8308daa35f0aef65cc336f8030957082
andriy-titov-coder <andreytitov19880811@gmail.com>
2026-02-06 00:43:37 +0200

    Add AI-powered multi-topic quiz for kids with personalization

--- src/bot.py
+++ src/bot.py
@@ -15,7 +15,8 @@ from config import BOT_TOKEN
 from database import init_db
 from handlers import (
     start, random, random_button, gpt, message_handler, talk, talk_button,
-    translator, translator_button, gpt_button, calc, calc_button, registration_button
+    translator, translator_button, gpt_button, calc, calc_button, registration_button,
+    quiz, quiz_button
 )
 
 
@@ -29,6 +30,7 @@ app.add_handler(CommandHandler("gpt", gpt))
 app.add_handler(CommandHandler("talk", talk))
 app.add_handler(CommandHandler("translator", translator))
 app.add_handler(CommandHandler("calc", calc))
+app.add_handler(CommandHandler("quiz", quiz))
 
 app.add_handler(CallbackQueryHandler(gpt_button, pattern='^start$'))
 app.add_handler(CallbackQueryHandler(random_button, pattern='^(random|start)$'))
@@ -39,6 +41,7 @@ app.add_handler(
     CallbackQueryHandler(translator_button, pattern='^translator.*|^start$')
 )
 app.add_handler(CallbackQueryHandler(calc_button, pattern='^calc_.*$'))
+app.add_handler(CallbackQueryHandler(quiz_button, pattern='^quiz_.*$'))
 app.add_handler(CallbackQueryHandler(registration_button, pattern='^registration_.*$'))
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
